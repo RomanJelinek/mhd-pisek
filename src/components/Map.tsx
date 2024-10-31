e211928746423de9f90c0c5997fc33732721d406
RomanJelinek <roman.jelinek91@gmail.com>
2024-10-31 15:53:33 +0100

    chore: deleted leaflet icon

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,19 +3,6 @@
 import React, { FC, useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
-// Nastavení ikony pro Leaflet
-// @ts-expect-error ...
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-});
 
 const MyLocationMarker: FC = () => {
   const [position, setPosition] = useState<[number, number] | null>(null);
