f7e884daabf7474aa6f69061f02aaec02bf5ca8e
RomanJelinek <roman.jelinek91@gmail.com>
2024-10-31 20:20:19 +0100

    feat: watching user location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,48 +3,51 @@
 import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
-import L, { LatLng, LatLngBounds } from 'leaflet';
+import L, { LatLng } from 'leaflet';
 
 export default function App() {
   function LocationMarker() {
     const [position, setPosition] = useState<LatLng | null>(null);
-    const [bbox, setBbox] = useState<string[]>([]);
-
     const map = useMap();
 
     useEffect(() => {
-      if (typeof window !== 'undefined') {
-        map.locate().on('locationfound', function (e: L.LocationEvent) {
-          setPosition(e.latlng);
-          map.flyTo(e.latlng, map.getZoom());
-          const radius = e.accuracy;
-          const circle = L.circle(e.latlng, radius);
-          circle.addTo(map);
-          setBbox((e.bounds as LatLngBounds).toBBoxString().split(','));
-        });
+      if (typeof window !== 'undefined' && navigator.geolocation) {
+        const watchId = navigator.geolocation.watchPosition(
+          (e) => {
+            const newLatLng = new L.LatLng(
+              e.coords.latitude,
+              e.coords.longitude
+            );
+            setPosition(newLatLng);
+            map.flyTo(newLatLng, map.getZoom());
+            const radius = e.coords.accuracy;
+            const circle = L.circle(newLatLng, { radius });
+            circle.addTo(map);
+          },
+          (error) => console.error('Chyba při sledování polohy:', error),
+          {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+          }
+        );
+
+        // Vyčištění watchPosition při odpojení komponenty
+        return () => navigator.geolocation.clearWatch(watchId);
       }
     }, [map]);
 
     return position === null ? null : (
       <Marker position={position}>
-        <Popup>
-          You are here. <br />
-          Map bbox: <br />
-          <b>Southwest lng</b>: {bbox[0]} <br />
-          <b>Southwest lat</b>: {bbox[1]} <br />
-          <b>Northeast lng</b>: {bbox[2]} <br />
-          <b>Northeast lat</b>: {bbox[3]}
-        </Popup>
+        <Popup>You are here.</Popup>
       </Marker>
     );
   }
 
   return (
     <MapContainer
-      key={new Date().getTime()}
       center={[49.1951, 16.6068]}
       zoom={13}
       scrollWheelZoom
