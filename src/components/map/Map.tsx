4e59b1ab46597aca647284dab5d2ba1a9f564cde
uhliko <ondrej.uhlik@vodafone.com>
2024-11-05 17:57:04 +0100

    bigger distance

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import React, { useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
-import LocationMarker from "./LocationMarker";
-import { progressData } from "@/game_data/constants";
-import { useLocation } from "@/context/LocationContext";
-import { useProgress } from "@/context/ProgressContext";
-import { ArrowMarker } from "@/components/markers";
-import Modal from "../Modal";
-import { useModalControl } from "@/hooks/useModalControl";
+import { ArrowMarker } from '@/components/markers';
+import { useLocation } from '@/context/LocationContext';
+import { useProgress } from '@/context/ProgressContext';
+import { progressData } from '@/game_data/constants';
+import { useModalControl } from '@/hooks/useModalControl';
 import { resolveModules } from '@/utils/resolveModules';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet/dist/leaflet.css';
+import { useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import Modal from '../Modal';
+import LocationMarker from './LocationMarker';
 
 const Map = () => {
   const { position } = useLocation();
@@ -32,7 +32,8 @@ const Map = () => {
   useEffect(() => {
     if (position) {
       const distance = position.distanceTo(goalPosition);
-      if (distance < 5 && !modalState.isOpen) {
+      console.log(distance);
+      if (distance < 20 && !modalState.isOpen) {
         openModal(modalContent);
       }
     }
@@ -47,7 +48,7 @@ const Map = () => {
         doubleClickZoom={false}
         scrollWheelZoom={false}
         touchZoom={false}
-        style={{ height: "100vh" }}
+        style={{ height: '100vh' }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
